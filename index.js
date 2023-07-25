b9dd0524a584d8e2ae674ce407ba3a326214a30b
Juno & Ice <70862998+JunoAndIce@users.noreply.github.com>
2023-07-25 16:34:44 -0500

    Added new error handling for square shape

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 // TODO: Inquire user for creating a logo
 // TODO: Save all inputs to create an SVG file using said parameters
 
-
+// inquirer modules 
 const inquirer = require('inquirer');
 const maxLength = require('inquirer-maxlength-input-prompt')
-const fs = require('fs');
-// const generateSVG = require('./generateSVG');
 
-const Shapes = require('./lib/shapes');
+const fs = require('fs');
+
+// Shape Objects 
 const Square = require('./lib/square');
 const Circle = require('./lib/circle');
 const Triangle = require('./lib/triangle');
+
+
+// const generateSVG = require('./generateSVG');
 
 inquirer.registerPrompt('max-input', maxLength);
 
@@ -46,19 +49,13 @@ const questions = [
 function init() {
   inquirer.prompt(questions)
   .then(function(answers){
-    const fileName =`${answers.shape}.svg`
-
-    console.log(fileName)
-
     createObject(answers);
-
-      // writeToFile(fileName, answers);
   });
 };
 
 function createObject(data) {
+  const fileName =`${data.shape}.svg`
   if (data.shape === 'Square'){
-
     const newLogo = new Square
     (
       'rect x="10" y="10" width="30" height="30"',
@@ -66,19 +63,41 @@ function createObject(data) {
       data.sColor,
       data.tColor
     )
-    
+    writeToFile(fileName, newLogo)
+  } else if (data.shape === 'Circle') {
+    const newLogo = new Circle
+    (
+      'circle cx="25" cy="75" r="20"',
+      data.text,
+      data.sColor,
+      data.tColor
+    )
+    writeToFile(fileName, newLogo)
+  } else if (data.shape === 'Triangle') {
+    const newLogo = new Triangle
+    (
+      'polygon points="250,60 100,400 400,400"',
+      data.text,
+      data.sColor,
+      data.tColor
+    )
+    writeToFile(fileName, newLogo)
   }
+  
 }
 
   // Create a function to write SVG file
   function writeToFile(fileName, data) {
-    fs.writeFile(fileName, generateSVG(data), error => {
-      if(error) {
-        return console.log(error);
-      } else{
-        console.log(`Your ${data.shape}.svg is ready!`)
-      }
-    });
+    
+    console.log(`Your ${fileName}.svg is ready!`)
+
+    // fs.writeFile(fileName, generateSVG(data), error => {
+    //   if(error) {
+    //     return console.log(error);
+    //   } else{
+    //     console.log(`Your ${data.shape}.svg is ready!`)
+    //   }
+    // });
   };
 
 // Function call to initialize app
